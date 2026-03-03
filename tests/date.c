33edccc182ee31427ec85fc889bff69ba057bd75
Borja Sancho Fernández <160739491+borjasf@users.noreply.github.com>
2026-03-03 17:52:34 +0100

    tests

---
+++ tests/date.c
@@ -0,0 +1,18 @@
+# include "types.h"
+# include "user.h"
+# include "date.h"
+
+int
+main (int argc, char *argv [])
+{
+  struct rtcdate r;
+
+  if (date(&r))
+  {
+    printf(2, "date failed\n");
+    exit();
+  }
+
+  // Pon aquí tu código para imprimir la fecha en el formato que desees
+  exit();
+}
